85f8f895e19d494c8ff5d24cb4e744193e82326c
Joe Pointon <joepointon2@gmail.com>
2022-12-05 15:34:45 +0000

    implemented verifyAnswer, runs after 2nd answer is chosen and logs result to console

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 export default function App() {
-  
+  const noTiles = 16;
+  const [answer1, setAnswer1] = useState(null);
+  const [answer2, setAnswer2] = useState(null);
+
+  const answers = [
+    [0, 1, 2, 3],
+    [5, 6, 7, 8],
+    [3, 2, 1, 0],
+    [8, 7, 6, 5],
+  ];
+
+  // array filled with false, values change to true if those tiles have been correctly guessed
+  const usersAnswers = Array(Math.sqrt(noTiles))
+    .fill(false)
+    .map(() => Array(Math.sqrt(noTiles)).fill(false));
+
+  function BoardTile({ id }) {
+    const x = id % Math.sqrt(noTiles);
+    const y = Math.floor(id / Math.sqrt(noTiles));
+    return (
+      <div className="board-tile" onClick={() => handleClick(x, y)}>
+        {answers[y][x]}
+      </div>
+    );
+  }
+
+  function renderTiles(count) {
+    return Array(count)
+      .fill(0)
+      .map((v, i) => <BoardTile key={i} id={i} />);
+  }
+
+  function handleClick(x, y) {
+    if (!answer1) {
+      setAnswer1([x, y]);
+    } else {
+      // break if choosing same tile
+      if (answer1[0] === x && answer1[1] === y) return;
+      setAnswer2([x, y]);
+    }
+  }
+
+  function verifyAnswers() {
+    if(answers[answer1[1]][answer1[0]] === answers[answer2[1]][answer2[0]]){
+      console.log(true)
+    }else{
+      console.log(false);
+    } 
+    setAnswer1(null);
+    setAnswer2(null);
+  }
 
 
-  return <div className="board-container">{renderTiles(9)}</div>;
-}
+  useEffect(() => {
+    // verify answers once second answer has been set in state
+    if (answer2) {
+      verifyAnswers();
+    }
+  }, [answer2]);
 
-function BoardTile({id}) {
-  return <div className="board-tile" onClick={()=>handleClick(id)}>Hello</div>;
-}
-
-function handleClick(tile){
-  console.log(tile);
-}
-
-function renderTiles(count) {
-  return Array(count)
-    .fill(0)
-    .map((v, i) => <BoardTile key={i} id={i}/>);
+  return <div className="board-container">{renderTiles(noTiles)}</div>;
 }
