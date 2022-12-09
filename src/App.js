c0731ba3d91e31d0fdb6f8889b42c8a3517c3b71
Joe Pointon <joepointon2@gmail.com>
2022-12-09 11:55:48 +0000

    turns are counted and scores are displayed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ export default function App() {
   const [answer1, setAnswer1] = useState(null);
   const [answer2, setAnswer2] = useState(null);
   const [visibilityTimeout, setVisibilityTimeout] = useState(null);
+  const [p1Score, setP1Score] = useState(0);
+  const [p2Score, setP2Score] = useState(0);
+  
+  // false for player 1, true for player 2
+  const [player1Turn, setPlayer1Turn] = useState(true);
   // array filled with false, values change to true if those tiles have been correctly guessed
   const [usersAnswers, setUsersAnswers] = useState(
     Array(Math.sqrt(noTiles))
@@ -71,6 +76,11 @@ export default function App() {
   function verifyAnswers() {
     if (answers[answer1[1]][answer1[0]] === answers[answer2[1]][answer2[0]]) {
       console.log(true);
+      if (player1Turn) {
+        setP1Score((prev) => prev + 1);
+      } else {
+        setP2Score((prev) => prev + 1);
+      }
       setUsersAnswers((prev) => {
         prev[answer1[1]][answer1[0]] = true;
         prev[answer2[1]][answer2[0]] = true;
@@ -92,10 +102,33 @@ export default function App() {
           setAnswer1(null);
           setAnswer2(null);
           setVisibilityTimeout(null);
+          setPlayer1Turn((prev) => !prev);
         }, 1000)
       );
     }
   }, [answer2]);
 
-  return <div className="board-container">{renderTiles(noTiles)}</div>;
+  useEffect(()=>{
+    
+  },usersAnswers)
+  return (
+    <>
+      <div className="title-container">
+        
+        <h1>Sqaure Guesser Game</h1>
+        <h3
+          className={
+            player1Turn ? "player-turn score-1" : "player-turn score-2"
+          }
+        >
+          Player {player1Turn ? 1 : 2}'s turn
+        </h3>
+        <div className="score-container">
+          <h3 className="score-1">P1 Score: {p1Score}</h3>
+          <h3 className="score-2">P2 Score: {p2Score}</h3>
+        </div>
+      </div>
+      <div className="board-container">{renderTiles(noTiles)}</div>
+    </>
+  );
 }
